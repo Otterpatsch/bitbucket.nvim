5786cf09d1059b88a3f0739a8f14ec5cdaaf13bb
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-02-14 14:11:56 +0100

    feat(commands): TEMP, drop me later!
    For debugging purposes a command which reloads the plugin

--- lua/bitbucket/commands.lua
+++ lua/bitbucket/commands.lua
@@ -2,3 +2,7 @@ vim.api.nvim_create_user_command("TestComments", function()
   package.loaded.bitbucket = nil
   require("bitbucket").get_comments(116)
 end, {})
+vim.api.nvim_create_user_command("Clear", function()
+  package.loaded.bitbucket = nil
+  require("bitbucket")
+end, {})
