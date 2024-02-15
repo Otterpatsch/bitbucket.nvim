2c549241bad9ad97f6234ca424e79b8fcfdc8346
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-02-15 10:03:21 +0100

    refactor: run stylua

--- lua/bitbucket/init.lua
+++ lua/bitbucket/init.lua
@@ -17,184 +17,184 @@ local Text = require("nui.text")
 local Tree = require("nui.tree")
 local NuiSplit = require("nui.split")
 
-
 function M.get_pull_requests()
-  -- get list of pull requests of PR
-  -- Just to show where are PR
-  -- and their corresponding branch
+	-- get list of pull requests of PR
+	-- Just to show where are PR
+	-- and their corresponding branch
 end
 
 function M.get_comments_by_commit(commithash)
-  local pr_id = M.get_pullrequest_by_commit(commithash)
-  local comments = M.get_comments(pr_id)
+	local pr_id = M.get_pullrequest_by_commit(commithash)
+	local comments = M.get_comments(pr_id)
 end
 
 function M.get_pullrequest_by_commit(commithash)
-  -- Get pullrequests by given commit if none is given?
-  -- Special chase if commit is present in multiple PRS
-  -- Then popup to choose PR
-  local commit = commithash or vim.fn.system("git rev-parse --short HEAD"):gsub("%W","")
-  local request_url = base_request_url .. "commit/" .. commit .. "/pullrequests"
-  local response = curl.get(request_url, {
-        accept = "application/json",
-        auth = username .. ":" .. app_password
-      })
-  local decoded_result = vim.fn.json_decode(response.body)
-  if #decoded_result["values"] ~= 1 then
-    error("two elements: handling yet not implemented")
-  else
-    return tostring( decoded_result["values"][1]["id"] )
-  end
+	-- Get pullrequests by given commit if none is given?
+	-- Special chase if commit is present in multiple PRS
+	-- Then popup to choose PR
+	local commit = commithash or vim.fn.system("git rev-parse --short HEAD"):gsub("%W", "")
+	local request_url = base_request_url .. "commit/" .. commit .. "/pullrequests"
+	local response = curl.get(request_url, {
+		accept = "application/json",
+		auth = username .. ":" .. app_password,
+	})
+	local decoded_result = vim.fn.json_decode(response.body)
+	if #decoded_result["values"] ~= 1 then
+		error("two elements: handling yet not implemented")
+	else
+		return tostring(decoded_result["values"][1]["id"])
+	end
 end
 
--- Function which visualize the overall Pull Request Comments 
+-- Function which visualize the overall Pull Request Comments
 -- Comments which are not put on some line of code/are linked to a specific file
 function M.get_comments(pr_id)
-  pr_id = pr_id or pull_request_id
-  local comment_split = NuiSplit({
-    ns_id = "comments",
-    relative = "editor",
-    position = "left",
-    size = "35%",
-  })
-  local request_url = base_request_url .. "/pullrequests/" .. pr_id .. "/comments"
-  local response = curl.get(request_url, {
-        accept = "application/json",
-        auth = username .. ":" .. app_password
-      })
-  local content = vim.fn.json_decode(response.body)
-  local values = content["values"]
-  while content["next"] do
-    request_url = content["next"]
-    response = curl.get(request_url, {
-        accept = "application/json",
-        auth = username .. ":" .. app_password
-    })
-    content = vim.fn.json_decode(response.body)
-    values = utils.concate_tables(values,content["values"])
-  end
-  local tree = Tree({
-        bufnr = comment_split.bufnr,
-        get_node_id = function(node)
-          -- this is telling NuiTree where we're storing the id
-          return node.id
-        end,
-        prepare_node = function(node)
-          local line = Line()
-          line:append(string.rep(" ", node:get_depth() - 1))
-          if node:has_children() then
-            line:append(node:is_expanded() and " " or " ", "SpecialChar")
-          else
-            line:append(" ")
-          end
-          line:append(node.author)
-          local lines = {line}
+	pr_id = pr_id or pull_request_id
+	local comment_split = NuiSplit({
+		ns_id = "comments",
+		relative = "editor",
+		position = "left",
+		size = "35%",
+	})
+	local request_url = base_request_url .. "/pullrequests/" .. pr_id .. "/comments"
+	local response = curl.get(request_url, {
+		accept = "application/json",
+		auth = username .. ":" .. app_password,
+	})
+	local content = vim.fn.json_decode(response.body)
+	local values = content["values"]
+	while content["next"] do
+		request_url = content["next"]
+		response = curl.get(request_url, {
+			accept = "application/json",
+			auth = username .. ":" .. app_password,
+		})
+		content = vim.fn.json_decode(response.body)
+		values = utils.concate_tables(values, content["values"])
+	end
+	local tree = Tree({
+		bufnr = comment_split.bufnr,
+		get_node_id = function(node)
+			-- this is telling NuiTree where we're storing the id
+			return node.id
+		end,
+		prepare_node = function(node)
+			local line = Line()
+			line:append(string.rep(" ", node:get_depth() - 1))
+			if node:has_children() then
+				line:append(node:is_expanded() and " " or " ", "SpecialChar")
+			else
+				line:append("-", "SpecialChar")
+			end
+			line:append(node.author)
+			local lines = { line }
 
-          for _, raw_line in ipairs(vim.split(node.text, "\n")) do
-            table.insert(lines, Line({ Text(raw_line) }))
-          end
-          return lines
-        end,
-      })
-  local node_by_id = {}
+			for _, raw_line in ipairs(vim.split(node.text, "\n")) do
+				table.insert(lines, Line({ Text(raw_line) }))
+			end
+			if not node:has_children() then
+				table.insert(lines, Line({}))
+			end
+			return lines
+		end,
+	})
+	local node_by_id = {}
 
-  local function add_node_to_tree(id)
-    local node = tree:get_node(id)
-    if node then
-      return
-    end
+	local function add_node_to_tree(id)
+		local node = tree:get_node(id)
+		if node then
+			return
+		end
 
-    node = node_by_id[id]
-    if not node then
-      return
-    end
+		node = node_by_id[id]
+		if not node then
+			return
+		end
 
-    local parent_id = node.parent_id
-    if parent_id and not tree:get_node(parent_id) then
-      -- ensure parent is added before the child
-      add_node_to_tree(parent_id)
-    end
-    tree:add_node(node, parent_id)
-  end
+		local parent_id = node.parent_id
+		if parent_id and not tree:get_node(parent_id) then
+			-- ensure parent is added before the child
+			add_node_to_tree(parent_id)
+		end
+		tree:add_node(node, parent_id)
+	end
 
-  for _, value in ipairs(values) do
-    local text = value["content"]["raw"]
-    local author = value["user"]["display_name"]
-    local id = tostring(value["id"]) -- id needs to be string
-    local parent_id = value["parent"] and tostring(value["parent"]["id"]) -- id needs to be string
-    local node = Tree.Node({
-      text = text,
-      author = author,
-      id = id,
-      parent_id = parent_id,
-    }, {})
-    node:expand()
-    node_by_id[id] = node
-  end
+	for _, value in ipairs(values) do
+		local text = value["content"]["raw"]
+		local author = value["user"]["display_name"]
+		local id = tostring(value["id"]) -- id needs to be string
+		local parent_id = value["parent"] and tostring(value["parent"]["id"]) -- id needs to be string
+		local node = Tree.Node({
+			text = text,
+			author = author,
+			id = id,
+			parent_id = parent_id,
+		}, {})
+		node:expand()
+		node_by_id[id] = node
+	end
 
+	for id in pairs(node_by_id) do
+		add_node_to_tree(id)
+	end
 
-  for id in pairs(node_by_id) do
-    add_node_to_tree(id)
-  end
+	--- key map actions ---
+	local map_options = { noremap = true, nowait = true }
+	--- collpase current node ---
+	comment_split:map("n", "h", function()
+		local node = tree:get_node()
 
-  --- key map actions ---
-  local map_options = { noremap = true, nowait = true }
-  --- collpase current node ---
-  comment_split:map("n", "h", function()
-    local node = tree:get_node()
+		if node:collapse() then
+			tree:render()
+		end
+	end, map_options)
+	----------------------------
 
-    if node:collapse() then
-      tree:render()
-  end
-  end, map_options)
-  ----------------------------
+	--- collpase all nodes ---
+	comment_split:map("n", "H", function()
+		local updated = false
 
-  --- collpase all nodes ---
-  comment_split:map("n", "H", function()
-    local updated = false
+		for _, node in pairs(tree.nodes.by_id) do
+			updated = node:collapse() or updated
+		end
 
-    for _, node in pairs(tree.nodes.by_id) do
-      updated = node:collapse() or updated
-    end
+		if updated then
+			tree:render()
+		end
+	end, map_options)
+	----------------------------
 
-    if updated then
-      tree:render()
-    end
-  end, map_options)
-  ----------------------------
+	-- expand current node
+	comment_split:map("n", "l", function()
+		local node = tree:get_node()
 
-  -- expand current node
-  comment_split:map("n", "l", function()
-    local node = tree:get_node()
+		if node:expand() then
+			tree:render()
+		end
+	end, map_options)
 
-    if node:expand() then
-      tree:render()
-    end
-  end, map_options)
+	--- expand all nodes ---
+	comment_split:map("n", "L", function()
+		local updated = false
 
-  --- expand all nodes ---
-  comment_split:map("n", "L", function()
-    local updated = false
+		for _, node in pairs(tree.nodes.by_id) do
+			updated = node:expand() or updated
+		end
+		if updated then
+			tree:render()
+		end
+	end, map_options)
+	---------------------
 
-    for _, node in pairs(tree.nodes.by_id) do
-      updated = node:expand() or updated
-    end
-    if updated then
-      tree:render()
-    end
-  end, map_options)
-  ---------------------
-
-  tree:render()
-  comment_split:mount()
-  return values
+	tree:render()
+	comment_split:mount()
+	return values
 end
 
-
 function M.create_pullrequest()
-  -- post method
-  request_url = base_request_url .. "pullrequests"
-  curl.post()
+	-- post method
+	request_url = base_request_url .. "pullrequests"
+	curl.post()
 end
 
 require("bitbucket.commands")
--- lua/bitbucket/utils.lua
+++ lua/bitbucket/utils.lua
@@ -2,48 +2,50 @@ local Popup = require("nui.popup")
 local M = {}
 
 M.center_popup = Popup({
-    position = "50%",
-    size = {
-      width = "80%",
-      height = "60%",
-    },
-    enter = true,
-    focusable = true,
-    border = {
-      style = "rounded",
-      text = {
-        top = "Request Content",
-        top_align = "center",
-      },
-    },
-  })
+	position = "50%",
+	size = {
+		width = "80%",
+		height = "60%",
+	},
+	enter = true,
+	focusable = true,
+	border = {
+		style = "rounded",
+		text = {
+			top = "Request Content",
+			top_align = "center",
+		},
+	},
+})
 
 function M.dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. M.dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+	if type(o) == "table" then
+		local s = "{ "
+		for k, v in pairs(o) do
+			if type(k) ~= "number" then
+				k = '"' .. k .. '"'
+			end
+			s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
+		end
+		return s .. "} "
+	else
+		return tostring(o)
+	end
 end
 
 function M.create_vertial_split()
-  vim.cmd('vsplit')
-  local active_window = vim.api.nvim_get_current_win()
-  local buffer_number = vim.api.nvim_create_buf(true, true)
-  vim.api.nvim_win_set_buf(active_window, buffer_number)
-  return buffer_number
+	vim.cmd("vsplit")
+	local active_window = vim.api.nvim_get_current_win()
+	local buffer_number = vim.api.nvim_create_buf(true, true)
+	vim.api.nvim_win_set_buf(active_window, buffer_number)
+	return buffer_number
 end
 
 function M.concate_tables(first, second)
-  for i=1,#second do
-      first[#first+1] = second[i]
-  end
-  return first
+	for i = 1, #second do
+		first[#first + 1] = second[i]
+	end
+	return first
 end
 
 return M
