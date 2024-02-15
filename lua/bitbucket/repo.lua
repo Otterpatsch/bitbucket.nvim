d91ecbba4f73ea5597f16b3c1bfd2690116741b0
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-02-15 19:14:14 +0100

    fix(setup): fix wrong lowercase instead of uppercase for enviroment variable

--- lua/bitbucket/repo.lua
+++ lua/bitbucket/repo.lua
@@ -1,7 +1,7 @@
 local M = {}
 local repo_remote = vim.fn.system("git remote -v")
 M.workspace = vim.env.WORKSPACE or string.match(repo_remote, ":(%S+)/")
-M.reposlug = vim.env.reposlug or string.match(repo_remote, "/(%S+).git")
+M.reposlug = vim.env.REPOSLUG or string.match(repo_remote, "/(%S+).git")
 M.app_password = vim.env.APP_PASSWORD
 M.username = vim.env.USER_NAME
 return M
