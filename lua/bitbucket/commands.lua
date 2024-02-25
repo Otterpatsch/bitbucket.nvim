5f1c89c40dae25d93474cc03c2c5a7654d3fd0d3
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-02-25 16:03:23 +0100

    fix: only create the diffview tab once

--- lua/bitbucket/commands.lua
+++ lua/bitbucket/commands.lua
@@ -1,3 +1,4 @@
+local diffview_lib = require("diffview.lib")
 local requests = require("bitbucket.requests.init")
 
 vim.api.nvim_create_user_command("TestComments", function()
@@ -9,7 +10,10 @@ vim.api.nvim_create_user_command("Clear", function()
 end, {})
 
 vim.api.nvim_create_user_command("PR", function()
-	vim.cmd("DiffviewOpen main")
-	require("bitbucket.comments.mapping").tabnr = vim.api.nvim_get_current_tabpage()
+	local view = diffview_lib.get_current_view()
+	if not view then
+		vim.cmd("DiffviewOpen main")
+		require("bitbucket.comments.mapping").tabnr = vim.api.nvim_get_current_tabpage()
+	end
 	requests.get_comments_by_commit()
 end, {})
