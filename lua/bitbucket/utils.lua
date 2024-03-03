f5c91d0101b2bff8a4a9c95f84ca0d44acf636bb
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-03-03 18:28:16 +0100

    style: stylelua

--- lua/bitbucket/utils.lua
+++ lua/bitbucket/utils.lua
@@ -2,46 +2,46 @@ local Popup = require("nui.popup")
 local M = {}
 
 function M.create_popup(titel, width, height)
-	return Popup({
-		position = "50%",
-		relative = "editor",
-		size = {
-			width = width or 100,
-			height = height or 30,
-		},
-		enter = true,
-		focusable = true,
-		border = {
-			style = "rounded",
-			text = {
-				top = titel,
-				top_align = "center",
-			},
-		},
-	})
+  return Popup({
+    position = "50%",
+    relative = "editor",
+    size = {
+      width = width or 100,
+      height = height or 30,
+    },
+    enter = true,
+    focusable = true,
+    border = {
+      style = "rounded",
+      text = {
+        top = titel,
+        top_align = "center",
+      },
+    },
+  })
 end
 
 function M.dump(o)
-	if type(o) == "table" then
-		local s = "{ "
-		for k, v in pairs(o) do
-			if type(k) ~= "number" then
-				k = '"' .. k .. '"'
-			end
-			s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
-		end
-		return s .. "} "
-	else
-		return tostring(o)
-	end
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
+      end
+      s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
 end
 
 function M.create_vertial_split()
-	vim.cmd("vsplit")
-	local active_window = vim.api.nvim_get_current_win()
-	local buffer_number = vim.api.nvim_create_buf(true, true)
-	vim.api.nvim_win_set_buf(active_window, buffer_number)
-	return buffer_number
+  vim.cmd("vsplit")
+  local active_window = vim.api.nvim_get_current_win()
+  local buffer_number = vim.api.nvim_create_buf(true, true)
+  vim.api.nvim_win_set_buf(active_window, buffer_number)
+  return buffer_number
 end
 
 --- Function which concatenates two tables together
@@ -49,18 +49,19 @@ end
 ---@param second  table: second table which will be added to the first table
 ---@return table: combined table
 function M.concate_tables(first, second)
-	for i = 1, #second do
-		first[#first + 1] = second[i]
-	end
-	return first
+  for i = 1, #second do
+    first[#first + 1] = second[i]
+  end
+  return first
 end
 
 ---Return os.time from a given ISO 8601 date string.
 ---@param date string ISO 8601 formatted date string
 ---@return integer os.time
 M.convert_date_string_to_timestamp = function(date)
-	local year, month, day, hour, min, sec = date:match("(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)")
-	return os.time({ year = year, month = month, day = day, hour = hour, min = min, sec = sec })
+  local year, month, day, hour, min, sec = date:match("(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)")
+  return os.time({ year = year, month = month, day = day, hour = hour, min = min, sec = sec })
 end
+
 
 return M
