45821b3877e9716e049d1116a6be52cc9b2963f5
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-03-09 11:10:31 +0100

    fix(indicator): indicators now set on jump to diff

--- lua/bitbucket/view/utils.lua
+++ lua/bitbucket/view/utils.lua
@@ -6,7 +6,7 @@ local signs = require("bitbucket.view.sign")
 
 ---Creates and sets the diagonstic signs for each file
 ---@param comments_by_file
-function M.setup_comment_indicators(file_path, comments)
+function M.setup_comment_indicators(file, comments)
 	if repo.tabnr == nil then
 		notify("Can't jump to Diffvew. Is it open?", vim.log.levels.ERROR)
 		return
@@ -18,35 +18,33 @@ function M.setup_comment_indicators(file_path, comments)
 		notify("Could not find Diffview view", vim.log.levels.ERROR)
 		return
 	end
-	local files = view.panel:ordered_file_list()
 	local layout = view.cur_layout
-	for _, file in ipairs(files) do
-		if file.path == file_path then
-			notify(file.path)
-			if not async_ok then
-				notify("Could not load Diffview async", vim.log.levels.ERROR)
-				return
-			end
-			async.await(view:set_file(file))
-			-- TODO if old line then layout a else layout b
-			-- layout.a.file.bufnr
+	for _, node in ipairs(comments) do
+		if not async_ok then
+			notify("Could not load Diffview async", vim.log.levels.ERROR)
+			return
+		end
+		async.await(view:set_file(file))
+		if node.inline.from ~= vim.NIL then
 			layout.a:focus()
 			local buffer_number = vim.api.nvim_get_current_buf()
 			local buffer_name = vim.api.nvim_buf_get_name(buffer_number)
-			signs.place_sign_comment(tostring(file.path), buffer_name, 1)
+			signs.place_sign_comment(buffer_name .. tostring(node.inline.from), buffer_name, node.inline.from)
+		elseif node.inline.to ~= vim.NIl then
 			layout.b:focus()
 			local buffer_number = vim.api.nvim_get_current_buf()
 			local buffer_name = vim.api.nvim_buf_get_name(buffer_number)
-			signs.place_sign_comment(tostring(file.path), buffer_name, 1)
-			break
+			signs.place_sign_comment(buffer_name .. tostring(node.inline.to), buffer_name, node.inline.to)
 		end
 	end
 end
 
 function M.get_root_nodes()
 	local root_nodes = {}
-	for _, node in ipairs(repo.comment_tree:get_nodes()) do
-		if not node:get_parent_id() then
+	local nodes = repo.comment_tree:get_nodes()
+
+	for _, node in ipairs(nodes) do
+		if not node:get_parent_id() and node.inline then
 			table.insert(root_nodes, node)
 		end
 	end
@@ -55,11 +53,11 @@ end
 
 function M.group_node_by_file(nodes)
 	local nodes_by_file = {}
-	for _, node in ipairs(nodes) do
-		if nodes[node.inline.file] then
-			table.insert(nodes[node.inline.file], node)
+	for _, node in pairs(nodes) do
+		if nodes_by_file[node.inline.file] then
+			table.insert(nodes_by_file[node.inline.file], node)
 		else
-			nodes[node.inline.file] = { node }
+			nodes_by_file[node.inline.file] = { node }
 		end
 	end
 	return nodes_by_file
