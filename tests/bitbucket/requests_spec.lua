6efa624b06650000445a6b687f7c5738aebae3b5
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-03-10 14:11:50 +0100

    feat: remove pr_id paramater out of functions as the id is saved in specific module

--- tests/bitbucket/requests_spec.lua
+++ tests/bitbucket/requests_spec.lua
@@ -1,5 +1,6 @@
 local requests = require("bitbucket.actions.comments")
 local general = require("bitbucket.actions.general")
+local repo = require("bitbucket.repo")
 
 describe("api comments", function()
 	local comment_id = nil
@@ -9,7 +10,8 @@ describe("api comments", function()
 			"Second Line",
 			"Third Line",
 		}
-		local response = requests.send_request_to_add_comment(nil, PR_ID, text)
+		repo.pr_id = PR_ID
+		local response = requests.send_request_to_add_comment(nil, text)
 		local body = vim.fn.json_decode(response.body)
 		local expected_comment = "First Line of Comment  \nSecond Line  \nThird Line"
 		comment_id = tostring(body["id"])
@@ -19,7 +21,8 @@ describe("api comments", function()
 	it("update comment", function()
 		local text = { "First Line", "Third Line" }
 		assert.are.same("string", type(comment_id))
-		local response = requests.send_request_to_update_comment(comment_id, PR_ID, text)
+		repo.pr_id = PR_ID
+		local response = requests.send_request_to_update_comment(comment_id, text)
 		local body = vim.fn.json_decode(response.body)
 		local expected_comment = "First Line  \nThird Line"
 		assert.are.same(200, response.status)
@@ -27,29 +30,33 @@ describe("api comments", function()
 	end)
 	it("reply to a comment", function()
 		local text = "Reply (Test)"
-		local response = requests.send_request_to_add_comment(comment_id, PR_ID, text)
+		repo.pr_id = PR_ID
+		local response = requests.send_request_to_add_comment(comment_id, text)
 		local body = vim.fn.json_decode(response.body)
 		assert.are.same(201, response.status)
 		assert.are.same("Reply (Test)", body.content.raw)
 		assert.are.same(comment_id, tostring(body.parent.id))
-		response = requests.send_request_to_delete_comment(tostring(body.id), PR_ID)
+		response = requests.send_request_to_delete_comment(tostring(body.id))
 		assert.are.same(204, response.status)
 	end)
 	it("delete comment", function()
-		local response = requests.send_request_to_delete_comment(comment_id, PR_ID)
+		repo.pr_id = PR_ID
+		local response = requests.send_request_to_delete_comment(comment_id)
 		assert.are.same(204, response.status)
 	end)
 	it("new_comment(Single Line)", function()
+		repo.pr_id = PR_ID
 		local text = "Single Line"
-		local response = requests.send_request_to_add_comment(nil, PR_ID, text)
+		local response = requests.send_request_to_add_comment(nil, text)
 		local body = vim.fn.json_decode(response.body)
 		local expected_comment = "Single Line"
 		assert.are.same(201, response.status)
 		assert.are.same(expected_comment, body.content.raw)
-		response = requests.send_request_to_delete_comment(body["id"], PR_ID)
+		response = requests.send_request_to_delete_comment(body["id"])
 		assert.are.same(204, response.status)
 	end)
 	it("get all comments", function()
+		repo.pr_id = PR_ID
 		local values, status = requests.request_comments_table(PR_ID)
 		assert.are.same("table", type(values))
 		assert.are.same(200, status)
