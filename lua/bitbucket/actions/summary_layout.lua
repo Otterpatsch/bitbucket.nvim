b93bbd7f2a191ba34820d518289730fe0e359e1d
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-03-10 19:05:39 +0100

    refactor(summary_layout): is now a function

--- lua/bitbucket/actions/summary_layout.lua
+++ lua/bitbucket/actions/summary_layout.lua
@@ -1,34 +1,42 @@
+M = {}
 local Popup = require("nui.popup")
 local Layout = require("nui.layout")
 
-local title_popup = Popup({
-	border = "single",
-	enter = true,
-	focusable = true,
-})
-local summary_popup = Popup({
-	border = "single",
-	focusable = true,
-})
-local info_popup = Popup({ border = "single", focusable = false })
+function M.create_summary_layout()
+	local title_popup = Popup({
+		border = "single",
+		enter = true,
+		focusable = true,
+	})
+	local summary_popup = Popup({
+		border = "single",
+		focusable = true,
+	})
+	local info_popup = Popup({
+		border = "single",
+		focusable = true,
+	})
 
-local layout = Layout(
-	{
-		position = "50%",
-		size = {
-			width = 100,
-			height = 40,
+	local layout = Layout(
+		{
+			position = "50%",
+			size = {
+				width = 100,
+				height = 40,
+			},
 		},
-	},
-	Layout.Box({
-		Layout.Box(title_popup, { size = "10%" }),
-		Layout.Box(summary_popup, { size = "60%" }),
-		Layout.Box(info_popup, { size = "30%" }),
-	}, { dir = "col" })
-)
-return {
-	layout = layout,
-	title_bufnr = title_popup.bufnr,
-	summary_bufnr = summary_popup.bufnr,
-	info_bufnr = info_popup.bufnr,
-}
+		Layout.Box({
+			Layout.Box(title_popup, { size = "8%" }),
+			Layout.Box(summary_popup, { size = "60%" }),
+			Layout.Box(info_popup, { size = "30%" }),
+		}, { dir = "col" })
+	)
+	return {
+		layout = layout,
+		title_bufnr = title_popup.bufnr,
+		summary_bufnr = summary_popup.bufnr,
+		info_bufnr = info_popup.bufnr,
+	}
+end
+
+return M
