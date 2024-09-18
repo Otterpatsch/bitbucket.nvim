84973005c51d9bb75e4320253ce0d9e562792627
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-09-18 16:39:16 +0200

    fix: add enviroment variables for test stuff

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,8 @@
 
 XDG_CONFIG_HOME=$(pwd)/.test-config
 export XDG_CONFIG_HOME
+export WORKSPACE=$TEST_WORKSPACE
+export REPOSLUG=$TEST_REPOSLUG
 
 source .secrets
 nvim --headless -c "PlenaryBustedDirectory tests/bitbucket/ {init = 'tests/init.lua', sequential = true, keep_going=false}"
