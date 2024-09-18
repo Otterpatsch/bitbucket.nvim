6af078cf4edd96a92f121020424cf462f4cc6662
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-09-18 16:40:46 +0200

    tests: add basic curl test

--- tests/bitbucket/requests/general_spec.lua
+++ tests/bitbucket/requests/general_spec.lua
@@ -2,13 +2,24 @@ local general = require("bitbucket.actions.general")
 local repo = require("bitbucket.repo")
 
 describe("api general", function()
+	local pr_id = "2"
+	it("curl", function()
+		local curl = require("plenary").curl
+		local request_url = repo.base_request_url
+		print(request_url)
+		local response = curl.get(request_url, {
+			accept = "application/json",
+			auth = repo.username .. ":" .. repo.app_password,
+		})
+		assert.are.same(200, response.status)
+	end)
 	it("get pullrequests by commithash", function()
 		local pullrequest_id, status = general.get_pullrequest_by_commit("3400495")
-		assert.are.same(PR_ID, pullrequest_id)
+		assert.are.same(pr_id, pullrequest_id)
 		assert.are.same(200, status)
 	end)
 	it("get summary of pullrequest", function()
-		repo.pr_id = PR_ID
+		repo.pr_id = pr_id
 		local got = general.get_pullrequest_summary()
 		local expected = {
 			title = "Tests/branch for tests",
