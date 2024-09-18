dceaa04c147d0becffd6405647e3196afc23d908
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-09-18 16:18:25 +0200

    fix: url

--- setup-tests.sh
+++ setup-tests.sh
@@ -6,5 +6,5 @@ PACK_DIR=./.test-config/nvim/pack/tests/start
 mkdir -p "$PACK_DIR"
 git clone https://github.com/nvim-lua/plenary.nvim.git "$PACK_DIR/plenary.nvim"
 git clone https://github.com/MunifTanjim/nui.nvim.git "$PACK_DIR/nui.nvim"
-git clone https://github.com/carriga/nvim-notify.git "$PACK_DIR/notify.nvim"
+git clone https://github.com/rcarriga/nvim-notify.git "$PACK_DIR/notify.nvim"
 git clone https://github.com/sindrets/diffview.nvim.git "$PACK_DIR/diffview.nvim"
